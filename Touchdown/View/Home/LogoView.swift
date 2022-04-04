2e11bd42f17cbd651487729edca6107b1440bd12
Vojin <dvojinovic38@gmail.com>
2022-04-04 12:23:09 +0200

    Implementation of NavigationBar for all sizes of iPhones

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,37 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Dusan Vojinovic on 4.4.22..
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } //: HSTACK
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
