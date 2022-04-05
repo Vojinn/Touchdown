bae343f287d28e8aae863ba6a0c902160ae61cb8
Vojin <dvojinovic38@gmail.com>
2022-04-05 16:00:35 +0200

    Implementation of another horizontal grid layout for brands

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Dusan Vojinovic on 5.4.22..
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
---
+++ Touchdown/View/Home/BrandItemView.swift
@@ -0,0 +1,36 @@
+//
+//  BrandItemView.swift
+//  Touchdown
+//
+//  Created by Dusan Vojinovic on 5.4.22..
+//
+
+import SwiftUI
+
+struct BrandItemView: View {
+    
+    //MARK: - PROPERTIES
+    
+    let brand: Brand
+    
+    //MARK: - BODY
+    var body: some View {
+        Image(brand.image)
+            .resizable()
+            .scaledToFit()
+            .padding()
+            .background(Color.white.cornerRadius(12))
+            .background(
+                RoundedRectangle(cornerRadius: 12).stroke(Color.gray, lineWidth: 1)
+            )
+    }
+}
+
+struct BrandItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandItemView(brand: brands[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}
