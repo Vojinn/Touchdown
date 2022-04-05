d92ce28d030037ac88addbbf400a48cb9896579b
Vojin <dvojinovic38@gmail.com>
2022-04-05 16:15:57 +0200

    Implementation of navigation bar, header and top part with animation for products detail view

---
+++ Touchdown/View/Detail/TopPartDetailView.swift
@@ -0,0 +1,51 @@
+//
+//  TopPartDetailView.swift
+//  Touchdown
+//
+//  Created by Dusan Vojinovic on 5.4.22..
+//
+
+import SwiftUI
+
+struct TopPartDetailView: View {
+    
+    //MARK: - PROPERTY
+    
+    @State private var isAnimating: Bool = false
+    
+    //MARK: - BODY
+    var body: some View {
+        HStack(alignment: .center, spacing: 6, content: {
+            // PRICE
+            VStack(alignment: .leading, spacing: 6, content: {
+                Text("Price")
+                    .fontWeight(.semibold)
+                
+                Text(sampleProduct.formattedPrice)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .scaleEffect(1.35, anchor: .leading)
+            })
+            .offset(y: isAnimating ? -50 : -75)
+            Spacer()
+            // PHOTO
+            Image(sampleProduct.image)
+                .resizable()
+                .scaledToFit()
+                .offset(y: isAnimating ? 0 : -35)
+        }) //: HSTACK
+        .onAppear(perform: {
+            withAnimation(.easeOut(duration: 0.75)) {
+                isAnimating.toggle()
+            }
+        })
+    }
+}
+
+struct TopPartDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopPartDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
